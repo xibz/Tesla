91a7c497488215f6696d816462034a34bc5c8932
xibz <impactbchang@gmail.com>
2012-09-08 04:30:45 -0700

    Added operator overloading of + and += for vector.h

--- Tesla/vector.h
+++ Tesla/vector.h
@@ -16,6 +16,14 @@ namespace Tesla
 				y *= c;
 			}
 			Vector2i operator*(const Int32 c) { return Vector2i(x*c, y*c); }
+			void operator+=(Vector2i& v)
+			{
+				x+=v.getX();
+				y+=v.getY();
+			}
+			Vector2i operator+(Vector2i v){ return Vector2i(x+v.getX(), y+v.getY()); }
+			Int32 getX() { return x; }
+			Int32 getY() { return y; }
 		private:
 			Int32 x, y;
 	};
@@ -32,6 +40,14 @@ namespace Tesla
 				y *= c;
 			}
 			Vector2f operator*(const Real c) { return Vector2f(x*c, y*c); }
+			void operator+=(Vector2f& v)
+			{
+				x+=v.getX();
+				y+=v.getY();
+			}
+			Vector2f operator+(Vector2f v){ return Vector2f(x+v.getX(), y+v.getY()); }
+			Real getX() { return x; }
+			Real getY() { return y; }
 		private:
 			Real x, y;
 	};
@@ -49,6 +65,16 @@ namespace Tesla
 				z *= c;
 			}
 			Vector3i operator*(const Int32 c) { return Vector3i(x*c, y*c, z*c); }
+			void operator+=(Vector3i& v)
+			{
+				x+=v.getX();
+				y+=v.getY();
+				z+=v.getZ();
+			}
+			Vector3i operator+(Vector3i v){ return Vector3i(x+v.getX(), y+v.getY(), z+v.getZ()); }
+			Int32 getX() { return x; }
+			Int32 getY() { return y; }
+			Int32 getZ() { return z; }
 		private:
 			Int32 x, y, z;
 	};
@@ -60,6 +86,16 @@ namespace Tesla
 			Vector3f(const Real x, const Real y, const Real z): x(x), y(y), z(z){}
 			Real magnitude() { return sqrt(x*x+y*y+z*z); }
 			Vector3f operator*(const Real c) { return Vector3f(x*c, y*c, z*c); }
+			void operator+=(Vector3f& v)
+			{
+				x+=v.getX();
+				y+=v.getY();
+				z+=v.getZ();
+			}
+			Vector3f operator+(Vector3f v){ return Vector3f(x+v.getX(), y+v.getY(), z+v.getZ()); }
+			Real getX() { return x; }
+			Real getY() { return y; }
+			Real getZ() { return z; }
 		private:
 			Real x, y, z;
 	};
