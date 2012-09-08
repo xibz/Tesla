08b01462411e6f8c9a7027ef288defdad8ee3af3
xibz <impactbchang@gmail.com>
2012-09-08 04:10:32 -0700

    Updated vector.h

--- Tesla/vector.h
+++ Tesla/vector.h
@@ -4,46 +4,62 @@
 #include <math.h>
 namespace Tesla
 {
-	class Vector
-	{
-		virtual Real magnitude() = 0;
-	};
-	class Vector2i:Vector
+	class Vector2i
 	{
 		public:
 			Vector2i():x(0), y(0){}
-			Vector2i(const uInt32 x, const uInt32 y): x(x), y(y){}
-			virtual Real magnitude() { return sqrt(x*x+y*y); }
+			Vector2i(const Int32 x, const Int32 y): x(x), y(y){}
+			Real magnitude() { return sqrt(x*x+y*y); }
+			void operator*=(const Int32 c)
+			{
+				x *= c;
+				y *= c;
+			}
+			Vector2i operator*(const Int32 c) { return Vector2i(x*c, y*c); }
 		private:
-			uInt32 x, y;
+			Int32 x, y;
 	};
 	
-	class Vector2f:Vector
+	class Vector2f
 	{
 		public:
 			Vector2f():x(0.0f), y(0.0f){}
 			Vector2f(const Real x, const Real y): x(x), y(y){}
-			virtual Real magnitude() { return sqrt(x*x+y*y); }
+			Real magnitude() { return sqrt(x*x+y*y); }
+			void operator*=(const Real c)
+			{
+				x *= c;
+				y *= c;
+			}
+			Vector2f operator*(const Real c) { return Vector2f(x*c, y*c); }
 		private:
 			Real x, y;
 	};
 	
-	class Vector3i:Vector
+	class Vector3i
 	{
 		public:
 			Vector3i():x(0), y(0), z(0){}
-			Vector3i(const uInt32 x, const uInt32 y, const uInt32 z): x(x), y(y), z(z){}
-			virtual Real magnitude() { return sqrt(x*x+y*y+z*z); }
+			Vector3i(const Int32 x, const Int32 y, const Int32 z): x(x), y(y), z(z){}
+			Real magnitude() { return sqrt(x*x+y*y+z*z); }
+			void operator*=(const Int32 c)
+			{
+				x *= c;
+				y *= c;
+				z *= c;
+			}
+			Vector3i operator*(const Int32 c) { return Vector3i(x*c, y*c, z*c); }
 		private:
-			uInt32 x, y, z;
+			Int32 x, y, z;
 	};
 	
-	class Vector3f:Vector
+	class Vector3f
 	{
 		public:
 			Vector3f():x(0.0f), y(0.0f), z(0.0f){}
 			Vector3f(const Real x, const Real y, const Real z): x(x), y(y), z(z){}
-			virtual Real magnitude() { return sqrt(x*x+y*y+z*z); }
+			Real magnitude() { return sqrt(x*x+y*y+z*z); }
+			Vector3f operator*(const Real c) { return Vector3f(x*c, y*c, z*c); }
 		private:
 			Real x, y, z;
 	};
